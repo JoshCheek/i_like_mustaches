4b17368079600277df474b3eb2240774f07d7153
Josh Cheek <josh.cheek@gmail.com>
2012-12-23 10:32:47 -0600

    Generalize specs so we can introduce apps alongside quicknotes

---
+++ spec/printing_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+require 'stringio'
+
+describe 'when printing' do
+  context 'quicknotes' do
+    it 'lines everything up all pretty-like, even multilines, and strips leading whitespace' do
+      qns = ILikeMustaches::QuickNote::Collection.new
+      qns.add 'key', 'value'
+      qns.add <<-KEY, 'and a value'
+        This is
+          some key
+      KEY
+      qns.add 'this is a long key', <<-VALUE
+          And a
+            multiline
+        value
+      VALUE
+      qns.add <<-KEY, <<-VALUE
+        multiline
+        key
+      KEY
+        multiline
+        value
+      VALUE
+
+      stdout  = StringIO.new
+      ILikeMustaches::Printer.new(qns, stdout).call
+
+      stdout.string.should ==
+<<OUT.gsub('|', '')
+key                   value|
+This is               and a value|
+  some key            |
+this is a long key      And a|
+                          multiline|
+                      value|
+multiline             multiline|
+key                   value|
+OUT
+    end
+
+    example 'apps' do
+      pending
+    end
+  end
+end
